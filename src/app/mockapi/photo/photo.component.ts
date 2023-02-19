56fc4908c059e144800b983c981a64b4a3e377d5
Feng Wang <globefeng@yahoo.com>
2023-02-19 08:35:02 -0600

    update photo row

--- src/app/mockapi/photo/photo.component.ts
+++ src/app/mockapi/photo/photo.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PhotoService } from './photo.service';
 import { PhotoInfo } from './photo';
+import { NgOptimizedImage } from '@angular/common'
 
 @Component({
   selector: 'app-photo',
